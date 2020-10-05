b65fb7bda25adc80f6f54e433083769d57737624
Thomas H <t.hanna3332@gmail.com>
2020-10-05 19:43:40 -0400

    added bootstrap and basic routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,90 @@
 import React, { useState, useEffect, useRef } from "react";
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Maps from './Screens/Maps'
+import home from './Screens/home.js';
+import {Navbar, Nav, NavDropdown, Form, FormControl, Button, Container} from 'react-bootstrap';
+
 
 class App extends React.Component {
   render() {
     return (
-      <Router basename="/">
+      <Router>
+      <div>
+        {/* <nav>
+          <ul>
+            <li>
+              <Link to="/">Home<home/>
+              </Link>
+            </li>
+            <li>
+              <Link to="/about">About<form/>
+              </Link>
+            </li>
+            <li>
+              <Link to="/users">Users<filler/>
+              </Link>
+            </li>
+          </ul>
+        </nav> */}
+     
+        <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+   
+  </Navbar.Collapse>
+</Navbar>
+
+
+
+
+
+
+
+      <Maps/>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            {/* <Container classname = "footer">
+
+            </Container> */}
         <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
           <Route path="/">
-            <Maps />
+            <Home />
           </Route>
         </Switch>
-      </Router>
-    );
-  }
+      </div>
+    </Router>
+  );
+}
+}
+function Home() {
+  return <h2></h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
