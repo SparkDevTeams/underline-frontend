3a47d3d5a57d1cc839b8d7deb2386ec4d9a67737
Juan Molina Jiron <jmolinaj@fiu.edu>
2021-03-09 15:59:54 -0500

    Added validation to component. Added red message box on of the input field detailing what the error message is.

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,12 +1,25 @@
 <template>
 	<div id="signup-component">
 		<h1>Signup</h1>
-
+		<div class="error-message-container" v-if="errorMessages.length > 0">
+			<p>The following errors occurred:</p>
+			<ul>
+				<li v-for="(message, index) in errorMessages" :key="index">
+					{{ message }}
+				</li>
+			</ul>
+		</div>
 		<label>First Name</label>
-		<input v-model="signUpData.firstName" @change="validateName" />
+		<input
+			v-model="signUpData.firstName"
+			@change="validateName(signUpData.firstName, 'first')"
+		/>
 
 		<label>Last Name</label>
-		<input v-model="signUpData.lastName" @change="validateName" />
+		<input
+			v-model="signUpData.lastName"
+			@change="validateName(signUpData.lastName, 'last')"
+		/>
 
 		<label>Email</label>
 		<input v-model="signUpData.email" @change="validateEmail" />
@@ -15,7 +28,7 @@
 		<input
 			v-model="signUpData.password"
 			type="password"
-			@change="validatePasswordsAreSame"
+			@change="validatePassword"
 		/>
 
 		<label>Confirm Password</label>
@@ -41,50 +54,90 @@ export default {
 				password: '',
 				confirmPassword: ''
 			},
-			hasError: false
+			errors: {
+				name: false,
+				email: false,
+				password: false,
+				confirmPassword: false,
+				emptyFields: false
+			},
+			hasError: false,
+			errorMessages: []
 		}
 	},
 	methods: {
-		validateName() {
-			//Checks if there is numbers on firstName var
-			if (!this.signUpData.firstName.match(/^([^0-9]*)$/)) {
-				console.log('Please check your first name.')
-				this.hasError = true
-			} else {
-				console.log('Your first name is valid.')
-				this.hasError = false
-			}
+		validateName(name, nameData) {
+			let insufficientLettersError = `${this.capitalize(
+				nameData
+			)} name must be between 2 to 36 characters`
+			let noNumbersError = `Please check your ${nameData} name does not contain numbers.`
+			//Checks if user's first/last name is within 2 - 36 char long
+			if (name.length >= 2 && name.length <= 36) {
+				//Removes error message from array
+				this.errorMessages = this.errorMessages.filter(
+					msg => msg !== insufficientLettersError
+				)
 
-			//Checks if there is numbers on lastName var
-			if (!this.signUpData.lastName.match(/^([^0-9]*)$/)) {
-				console.log('Please check your last name.')
-				this.hasError = true
+				//Checks if text field contains numbers
+				if (!name.match(/^([^0-9]*)$/)) {
+					this.errorMessages.push(noNumbersError)
+					this.errors.name = true
+				} else {
+					//Removes error message from array
+					this.errorMessages = this.errorMessages.filter(
+						msg => msg !== noNumbersError
+					)
+					this.errors.name = false
+				}
 			} else {
-				console.log('Your last name is valid.')
-				this.hasError = false
+				this.errorMessages.push(insufficientLettersError)
+				this.errors.name = true
 			}
 		},
 		validateEmail() {
+			let errorMessage = 'Please provide valid email.'
 			//Checks email format is correct.
 			if (!this.signUpData.email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
-				console.log('Please provide valid email.')
-				this.hasError = true
+				this.errorMessages.push(errorMessage)
+				this.errors.email = true
 			} else {
-				console.log('Your email is valid.')
-				this.hasError = false
+				this.errorMessages = this.errorMessages.filter(
+					msg => msg !== errorMessage
+				)
+				this.errors.email = false
+			}
+		},
+		validatePassword() {
+			let errorMessage =
+				'Please make sure your password is between 6 to 15 characters long.'
+			if (
+				this.signUpData.password.length >= 6 &&
+				this.signUpData.password.length <= 15
+			) {
+				this.errorMessages = this.errorMessages.filter(
+					msg => msg !== errorMessage
+				)
+				this.errors.password = false
+			} else {
+				this.errorMessages.push(errorMessage)
+				this.errors.password = true
 			}
 		},
 		validatePasswordsAreSame() {
+			let errorMessage = 'Please check your passwords match'
 			//Checks if passwords match
 			if (this.signUpData.password !== this.signUpData.confirmPassword) {
-				console.log('Please check your passwords match')
-				this.hasError = true
+				this.errorMessages.push(errorMessage)
+				this.errors.confirmPassword = true
 			} else {
-				console.log('Your passwords match')
-				this.hasError = false
+				this.errorMessages = this.errorMessages.filter(
+					msg => msg !== errorMessage
+				)
+				this.errors.confirmPassword = false
 			}
 		},
 		validate() {
+			let errorMessage = 'Please make sure no text field is left blank'
 			//Checks if inputs are empty
 			if (
 				this.signUpData.firstName == '' ||
@@ -94,22 +147,33 @@ export default {
 				this.signUpData.password == '' ||
 				this.signUpData.confirmPassword == ''
 			) {
-				console.log('Something went wrong, please check input.')
-				this.hasError = true
+				this.errorMessages.push(errorMessage)
+				this.errors.emptyFields = true
 			} else {
-				this.hasError = false
+				this.errorMessages = this.errorMessages.filter(
+					msg => msg !== errorMessage
+				)
+				this.errors.emptyFields = false
+			}
+
+			for (const key in this.errors) {
+				if (this.errors[key] == true) {
+					this.hasError = true
+				}
 			}
 
 			//if hasError is false then it will return true
 			if (!this.hasError) {
+				console.log('Everything checks out.')
 				return true
 			}
 		},
 		signup() {
+			console.log(this.errors, this.hasError)
 			if (this.validate()) {
 				axios({
 					method: 'post',
-					url: 'https://sparkdev-underline.herokuapp.com/users/register',
+					url: '/users/register',
 					data: this.signUpData
 				})
 					.then(response => {
@@ -119,6 +183,10 @@ export default {
 						console.log(e)
 					})
 			}
+		},
+		capitalize(s) {
+			if (typeof s !== 'string') return ''
+			return s.charAt(0).toUpperCase() + s.slice(1)
 		}
 	},
 	mounted() {}
@@ -130,12 +198,31 @@ export default {
 #signup-component {
 	@extend .shadow;
 	background-color: white;
-	height: min(500px, 45vh);
+	height: auto;
 	width: min(500px, 45vw);
 	border-radius: 15px;
 	padding: 15px;
 	box-sizing: border-box;
 	@extend .flex-column;
 	margin: 5vw;
+
+	button {
+		@extend .button;
+		margin-top: 10px;
+		background: color(green);
+		width: 10vw;
+		height: 4vh;
+		border-radius: 5px;
+		font-family: $font;
+		letter-spacing: 1px;
+	}
+
+	.error-message-container {
+		@extend .flex-column;
+		background: rgba(255, 0, 0, 0.4);
+		width: 300px;
+		border-radius: 5px;
+		margin-bottom: 10px;
+	}
 }
 </style>
