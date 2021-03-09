6238c2100f68f3968fafc9fc2ba2c45edcc3b04c
Juan Molina Jiron <jmolinaj@fiu.edu>
2021-03-08 20:43:36 -0500

    Added simple validation on all user input. Needs further validation to check length of inputs.

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -3,19 +3,27 @@
 		<h1>Signup</h1>
 
 		<label>First Name</label>
-		<input v-model="firstName" @change="isValid" />
+		<input v-model="signUpData.firstName" @change="validateName" />
 
 		<label>Last Name</label>
-		<input v-model="lastName" />
+		<input v-model="signUpData.lastName" @change="validateName" />
 
 		<label>Email</label>
-		<input v-model="email" />
+		<input v-model="signUpData.email" @change="validateEmail" />
 
 		<label>Password</label>
-		<input v-model="password" type="password" />
+		<input
+			v-model="signUpData.password"
+			type="password"
+			@change="validatePasswordsAreSame"
+		/>
 
 		<label>Confirm Password</label>
-		<input v-model="confirmPassword" type="password" />
+		<input
+			v-model="signUpData.confirmPassword"
+			type="password"
+			@change="validatePasswordsAreSame"
+		/>
 
 		<button @click="signup">Submit</button>
 	</div>
@@ -26,27 +34,83 @@ import axios from 'axios'
 export default {
 	data() {
 		return {
-			firstName: '',
-			lastName: '',
-			email: '',
-			password: '',
-			confirmPassword: ''
+			signUpData: {
+				firstName: '',
+				lastName: '',
+				email: '',
+				password: '',
+				confirmPassword: ''
+			},
+			hasError: false
 		}
 	},
 	methods: {
-		isValid() {},
-		signup() {
-			if (this.isValid()) {
-				const signUpData = {}
+		validateName() {
+			//Checks if there is numbers on firstName var
+			if (!this.signUpData.firstName.match(/^([^0-9]*)$/)) {
+				console.log('Please check your first name.')
+				this.hasError = true
+			} else {
+				console.log('Your first name is valid.')
+				this.hasError = false
+			}
 
-				signUpData.firstName = this.firstName
-				signUpData.lastName = this.firstName
-				signUpData.email = this.firstName
-				signUpData.password = this.firstName
+			//Checks if there is numbers on lastName var
+			if (!this.signUpData.lastName.match(/^([^0-9]*)$/)) {
+				console.log('Please check your last name.')
+				this.hasError = true
+			} else {
+				console.log('Your last name is valid.')
+				this.hasError = false
+			}
+		},
+		validateEmail() {
+			//Checks email format is correct.
+			if (!this.signUpData.email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+				console.log('Please provide valid email.')
+				this.hasError = true
+			} else {
+				console.log('Your email is valid.')
+				this.hasError = false
+			}
+		},
+		validatePasswordsAreSame() {
+			//Checks if passwords match
+			if (this.signUpData.password !== this.signUpData.confirmPassword) {
+				console.log('Please check your passwords match')
+				this.hasError = true
+			} else {
+				console.log('Your passwords match')
+				this.hasError = false
+			}
+		},
+		validate() {
+			//Checks if inputs are empty
+			if (
+				this.signUpData.firstName == '' ||
+				this.signUpData.lastName == '' ||
+				this.signUpData.email == '' ||
+				this.signUpData.password == '' ||
+				this.signUpData.password == '' ||
+				this.signUpData.confirmPassword == ''
+			) {
+				console.log('Something went wrong, please check input.')
+				this.hasError = true
+			} else {
+				this.hasError = false
+			}
+
+			//if hasError is false then it will return true
+			if (!this.hasError) {
+				return true
+			}
+		},
+		signup() {
+			if (this.validate()) {
 				axios({
 					method: 'post',
 					url: 'https://sparkdev-underline.herokuapp.com/users/register',
-					data: signUpData
+					data: this.signUpData
 				})
 					.then(response => {
 						console.log(response)
