2abf20857024bcdb7d8fbabf319765bff7df6bf9
Juan Molina Jiron <jmolinaj@fiu.edu>
2021-03-15 18:30:53 -0400

    created branch

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
 <template>
-  <div id="app">
-    <NavBar></NavBar>
-    <router-view></router-view>
-  </div>
+	<div id="app">
+		<NavBar></NavBar>
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
-import NavBar from "./components/NavBar.vue";
+import NavBar from './components/NavBar.vue'
 export default {
-  components: {
-    NavBar,
-  },
-};
+	components: {
+		NavBar
+	},
+	created() {
+		//if localstorage variable exists, make request to validate token end point
+		//if doesnt exist make the the token variable empty
+	}
+}
 </script>
 
 <style lang="scss">
-@import "./assets/global.scss";
-@import "./assets/fonts.css";
+@import './assets/global.scss';
+@import './assets/fonts.css';
 
 #app {
-  @extend .clear;
-  font-family: $font;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  height: 100vh;
-  width: 100vw;
-  @extend .flex-column;
-  justify-content: flex-start; 
+	@extend .clear;
+	font-family: $font;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	height: 100vh;
+	width: 100vw;
+	@extend .flex-column;
+	justify-content: flex-start;
 }
 </style>
