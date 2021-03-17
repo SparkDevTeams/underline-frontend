2dc52d9717199947f7c57908fe9d915fc97217c7
CharlotteW923 <CharlotteW923@gmail.com>
2021-03-17 15:03:41 -0400

    Created Accept and Remove Buttons. Updated layout of event containers to be horizontal. Changed styling of buttons

--- src/components/AdminEvent.vue
+++ src/components/AdminEvent.vue
@@ -1,11 +1,22 @@
 <template>
   <div id = "admin-event-component">
+      <img :src ="event-image">
+      <h2>{{placeholderTitle}}</h2>
+      <p>Description of event</p>
+      <button id = "remove-button">Remove</button>
+      <button id ="accept-button">Accept</button>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
+  name: "AdminEvent",
+  props: [
+      "placeholderTitle",
+      "event-image"
+    ],
+
      data () {
           return {
   
@@ -26,6 +37,60 @@ export default {
 @import "../assets/global.scss";
 
 #admin-event-component{
+  font-family: $font;
+  @extend .clear;
+  width: 1000px;
+  height: 50%;
+  z-index: 401;
+  padding: 2%;
+  box-sizing: border-box;
+  margin-top: 10px;
+
+  background: white;
+  border-radius: 10px;
+  @extend .shadow;
+  @extend .flex-row;
+
+  h2 {
+    color:color(green);
+    font-size: 20px;
+    margin-right: auto;
+    @extend .clear;
+    width: 150px;
+  }
+
+  p {
+    font-size: 17px;
+    font-weight: 250;
+    margin-right: auto;
+  }
+  #accept-button {
+    @extend .button;
+    padding: 8px 15px;
+    border-radius: 5px;
+    border-style: solid;
+    font-family: $font;
+    color: green;
+    font-size: 15px;
+    margin-top: 15px;
+  }
+
+  #remove-button{
+      @extend .button;
+      padding: 8px 15px;
+      border-radius: 5px;
+      color: grey;
+      font-size: 15px;
+      margin-top: 15px;
+
+  }
+
+  img{
+    width: 50px;
+    height: 50px;
+    border-radius: 15px;
+    margin-right: auto;
+  }
  
 
 }
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,7 +1,8 @@
 <template>
   <div id = "admin-component">
        Admin
-       <AdminEvent></AdminEvent>
+       <h2>Events for Review</h2>
+       <AdminEvent v-for="EventTitle in EventList" :placeholderTitle ="EventTitle" :event-image="EventImage"></AdminEvent>
   </div>
 </template>
 
@@ -9,8 +10,11 @@
 import axios from "axios";
 import AdminEvent from "../components/AdminEvent.vue";
 export default {
+     name: "Admin",
      data () {
           return {
+               EventList: ["Title 1", "Title 2", "Title 3", "Title 4", "Title 5", "Title 6"],
+               EventImage:["https://www.explorebranson.com/sites/default/files/styles/hero/public,/articles/dogwood-canyon-nature-park.jpg?itok=T6-PdQKC"]
   
           }
      },
@@ -30,7 +34,7 @@ export default {
 @import "../assets/global.scss";
 
 #admin-component{
-     
+     height: 20vh;
 }
 
 </style>
