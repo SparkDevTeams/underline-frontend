156e0dcaf2083eb85464ac38629cbda39cebf75b
Juan Molina Jiron <jmolinaj@fiu.edu>
2021-03-17 22:08:58 -0400

    Added styling to Edit Event component. Very minimal styling.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,64 +1,63 @@
 <template>
-     <div id="nav-container">
-          <img id="logo" src="../assets/grn-and-wht-u-logo.png" />
-          <router-link to="/">Explore</router-link>
-          <button>Events</button>
-          <router-link to="About">About</router-link>
-          <button id="support-button">Support</button>
-          <router-link to="Signin">Sign in</router-link>
-     </div>
+	<div id="nav-container">
+		<img id="logo" src="../assets/grn-and-wht-u-logo.png" />
+		<router-link to="/">Explore</router-link>
+		<button>Events</button>
+		<router-link to="About">About</router-link>
+		<button id="support-button">Support</button>
+		<router-link to="Signin">Sign in</router-link>
+	</div>
 </template>
 
 <script>
 export default {
-     data() {
-          return {};
-     },
-     methods: {
-
-     },
-};
+	data() {
+		return {}
+	},
+	methods: {}
+}
 </script>
 
 <style lang="scss" scoped>
-@import "../assets/global.scss";
-@import "../assets/fonts.css";
+@import '../assets/global.scss';
+@import '../assets/fonts.css';
 
 #nav-container {
-     font-family: $font;
-     @extend .flex-row;
-     justify-content: flex-start;
-     height: 11vh;
-     min-height: 11vh;
-     max-height: 11vh;
-     width: 100vw;
-     background-color: rgb(0, 0, 0);
-     padding: 0px 20px;
-     box-sizing: border-box;
+	font-family: $font;
+	@extend .flex-row;
+	justify-content: flex-start;
+	height: 11vh;
+	min-height: 11vh;
+	max-height: 11vh;
+	width: 100vw;
+	background-color: rgb(0, 0, 0);
+	padding: 0px 20px;
+	box-sizing: border-box;
 
-     #logo {
-          height: 60%;
-     }
+	#logo {
+		height: 60%;
+	}
 
-     button,a {
-          text-decoration: none;       
-          @extend .clear;
-          font-smooth: antialiased;
-          font-family: $font;
-          background-color: transparent;
-          font-size: 19px;
-          font-weight: 600;
-          color: gray;
-          margin: 0px 25px;
-          cursor: pointer;
+	button,
+	a {
+		text-decoration: none;
+		@extend .clear;
+		font-smooth: antialiased;
+		font-family: $font;
+		background-color: transparent;
+		font-size: 19px;
+		font-weight: 600;
+		color: gray;
+		margin: 0px 25px;
+		cursor: pointer;
 
-          &:hover {
-               color: rgb(255, 255, 255);
-          }
-     }
+		&:hover {
+			color: rgb(255, 255, 255);
+		}
+	}
 
-     #support-button {
-          margin-left: auto;
-     }
+	#support-button {
+		margin-left: auto;
+	}
 }
 </style>
--- src/views/EditEvent.vue
+++ src/views/EditEvent.vue
@@ -1,26 +1,152 @@
 <template>
-  <div>edit event test</div>
+	<div id="edit-event-container">
+		<h1>Create your event</h1>
+		<div class="input-wrapper">
+			<label>Event name:</label>
+			<input type="text" id="event-name" />
+		</div>
+
+		<div class="input-wrapper">
+			<label>Event description:</label>
+			<textarea id="event-description"></textarea>
+		</div>
+
+		<div class="input-wrapper">
+			<label>Event start time:</label>
+			<input type="datetime-local" id="event-start" />
+		</div>
+
+		<div class="input-wrapper">
+			<label>Event end time</label>
+			<input type="datetime-local" id="event-end" />
+		</div>
+
+		<div class="input-wrapper">
+			<label>Tags:</label>
+			<textarea id="event-tags"></textarea>
+		</div>
+
+		<div class="input-wrapper">
+			<label>Event location:</label>
+			<input type="text" id="event-location" />
+		</div>
+
+		<div class="input-wrapper">
+			<label>Max Attendees:</label>
+			<input type="number" id="event-max" />
+		</div>
+
+		<div class="input-wrapper">
+			<label>Website link:</label>
+			<input type="text" id="event-link" />
+		</div>
+
+		<button type="submit" @click="onSubmit">Submit</button>
+	</div>
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios'
 export default {
-  data() {
-    return {
-      id: this.$route.params.id,
-    };
-  },
-  methods: {},
-  mounted() {},
-  components: {},
-  watch: {
-    $route(to, from) {
-      this.id = this.$route.params.id;
-    },
-  },
-};
+	data() {
+		return {
+			id: this.$route.params.id
+		}
+	},
+	methods: {
+		onSubmit() {
+			console.log('Submitted')
+		}
+	},
+	mounted() {},
+	components: {},
+	watch: {
+		$route(to, from) {
+			this.id = this.$route.params.id
+		}
+	}
+}
 </script>
 
 <style lang="scss" scoped>
-@import "../assets/global.scss";
+@import '../assets/global.scss';
+#edit-event-container {
+	@extend .flex-column;
+	box-shadow: 6px 9px 0px 1px #000000;
+	width: 80vw;
+	height: 80vh;
+	border-radius: 5px;
+	border: 1px solid black;
+	margin-top: 50px;
+
+	h1 {
+		color: color(green);
+		border-bottom: 5px solid black;
+		cursor: default;
+		font-size: 40px;
+	}
+
+	button {
+		@extend .button;
+		margin-top: 20px;
+		background: color(green);
+		border-radius: 5px;
+		width: 200px;
+		height: 50px;
+		font-size: 20px;
+		font-weight: 300;
+	}
+
+	.input-wrapper {
+		@extend .flex-row;
+		width: 80%;
+		justify-content: space-around;
+		margin: 5px 0;
+
+		label {
+			padding: 0;
+			text-align: left;
+			width: 100%;
+			font-size: 26px;
+			letter-spacing: 1.5px;
+		}
+
+		input {
+			width: 500px;
+			height: 20px;
+			border: none;
+			border-bottom: 2px solid black;
+		}
+
+		input#event-max {
+			width: 80px;
+		}
+
+		textarea {
+			resize: none;
+			width: 500px;
+			height: 100px;
+			border: 2px solid black;
+		}
+	}
+}
+@media (min-width: 1000px) {
+	#edit-event-container {
+		width: 70vw;
+
+		h1 {
+			font-size: 48px;
+		}
+	}
+}
+@media (min-width: 1250px) {
+	#edit-event-container {
+		width: 60vw;
+	}
+}
+@media (min-width: 1500px) {
+	#edit-event-container {
+		width: 50vw;
+	}
+}
 </style>
