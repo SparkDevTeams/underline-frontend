63e3d14fff3be6cbc9b81f6639e4579b83a06bd3
CharlotteW923 <CharlotteW923@gmail.com>
2021-03-22 22:54:45 -0400

    Started writing description for the about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+       <h2>About Us</h2>
+          <p>The Underline is a 10-mile linear park, urban trail, and public art destination below 
+            Miami's Metrorail
+          </p>
+
+          <p>This web application serves as a map companion that will help park-goers navigate, and give them
+            information about events on the trail. <br> Guests will be able to create an account and make public or
+            private events they can share with their family and friends.
+          </p>
   </div>
 </template>
 
@@ -25,4 +34,7 @@ export default {
 <style lang="scss" scoped>
 @import "../assets/global.scss";
 
+h2{
+     color: green;
+}
 </style>
