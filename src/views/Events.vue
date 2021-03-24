f4e20de444d971b9d4f243b01b31f50e03c11a73
nathyhz <nhern117@fiu.edu>
2021-03-24 18:25:02 -0400

    Added a bit more styling

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="events-component">
-       <h1 id="title"><span>Events</span></h1>
+       <h1 id="page-title">Events</h1>
+       <hr class="solid">
        <section id="ongoing-events">
-            <h2><span>Check</span> out these events!</h2>
+            <h2 id="ongoing-events-title">Check out these events!</h2>
             <EventDisplay></EventDisplay>
        </section>
        <section id="upcoming-events">
-            <h2><span>Ongoing</span> Events</h2>
+            <h2 id="upcoming-events-title">Ongoing Events</h2>
             <EventDisplay></EventDisplay>
        </section>
   </div>
@@ -37,21 +38,41 @@ export default {
 @import "../assets/global.scss";
 
 #events-component{
-     #title{
-          
-     }
-     span{
-          color: #03bf4d;
-          border-top: 6px solid black;
-          line-height: 80px;
-          font-size: 50px;
+     @extend .clear;
+     height: 48vh;
+     width: 100%;
 
+     hr.solid{
+          border-top: 1px solid rgb(221, 221, 221);
+          width: 80%;
+     }
+
+     #page-title{
+          margin: auto;
+          margin-top: 30px;
+          font-size: 45px;
+          border-top: 6px solid black;
+          max-width: 11.50%;
+          color: #03bf4d;
+          text-align: center;
+     }
+     h2{
+          color: #03bf4d;
+          border-bottom: 6px solid black;
      }
      #ongoing-events{
+          padding-top: 10px;
+          margin-left: 20px;
+          text-align: center;
+          width: 23%;
      }
 
      #upcoming-events{
-     }
+          margin-left: 20px;
+          text-align: center;
+          width: 15%;
+          padding-bottom: 30px;
+          }
 }
 
 
