83816209608ab8c403a0f0896e14caf88017ec3c
Juan Molina Jiron <jmolinaj@fiu.edu>
2021-04-05 19:45:20 -0400

    Installed hamburger menu library. Added hamburger to App component and set it to display when screen size is less than 700px wide. Styled it to match application. Removed FontAwesome script from html since it will no longer be used.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,64 @@
 <template>
 	<div id="app">
-		<NavBar :loggedIn="signedIn"></NavBar>
+		<div id="nav-container" v-if="windowSize.width <= 700">
+			<Slide width="200" :closeOnNavigation="true" noOverlay>
+				<img id="logo" src="./assets/grn-and-wht-u-logo.png" />
+				<router-link class="menu-item" to="/">Explore</router-link>
+				<router-link class="menu-item" to="About">About</router-link>
+				<a
+					class="menu-item"
+					href="https://www.theunderline.org/support/"
+					id="support-button"
+					>Support</a
+				>
+				<router-link
+					class="menu-item"
+					v-if="isAdmin"
+					id="admin-portal"
+					to="admin"
+					>Admin Portal</router-link
+				>
+				<router-link
+					class="menu-item"
+					v-if="signedIn"
+					id="profile-page"
+					to="/user/ + id"
+					>Profile Page</router-link
+				>
+				<router-link class="menu-item" v-if="!signedIn" to="/signin"
+					>Sign in</router-link
+				>
+				<router-link class="menu-item" to="/" v-if="signedIn" @click="logout"
+					>Sign out</router-link
+				>
+			</Slide>
+		</div>
+
+		<NavBar v-if="windowSize.width > 700" :loggedIn="signedIn"></NavBar>
 		<router-view v-on:signedIn="signedIn = true"></router-view>
 	</div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue'
+import { Slide } from 'vue-burger-menu'
 import axios from 'axios'
+import jwt_decode from 'jwt-decode'
 export default {
 	data() {
 		return {
-			signedIn: false
+			signedIn: false,
+			windowSize: {
+				width: 0,
+				height: 0
+			},
+			id: '',
+			isAdmin: false
 		}
 	},
 	components: {
-		NavBar
+		NavBar,
+		Slide
 	},
 	created() {
 		if (window.localStorage.getItem('token')) {
@@ -38,6 +81,57 @@ export default {
 		} else {
 			window.localStorage.setItem('token', '')
 		}
+		//checks window size
+		window.addEventListener('resize', this.handleResize)
+		this.handleResize()
+	},
+	destroyed() {
+		window.removeEventListener('resize', this.handleResize)
+	},
+	methods: {
+		handleResize() {
+			this.windowSize.width = window.innerWidth
+			this.windowSize.height = window.innerHeight
+		},
+		checkUserSignedIn() {
+			const token = window.localStorage.getItem('token')
+			if (token != '') {
+				this.id = jwt_decode(token).user_id
+				this.signedIn = true
+				this.checkUserAdmin()
+			}
+		},
+		login() {
+			this.$router.push('/signin')
+		},
+		logout() {
+			window.localStorage.setItem('token', '')
+			this.signedIn = false
+			this.$router.push('/signin')
+		},
+		checkUserAdmin() {
+			axios({
+				method: 'post',
+				url: 'https://sparkdev-underline.herokuapp.com/users/find',
+				data: {
+					user_id: this.id
+				}
+			})
+				.then(response => {
+					if (response.data.user_type == 'ADMIN') {
+						this.isAdmin = true
+					}
+				})
+				.catch(error => {})
+		}
+	},
+	mounted() {
+		this.checkUserSignedIn()
+	},
+	watch: {
+		loggedIn: function(newVal, oldVal) {
+			this.signedIn = newVal
+		}
 	}
 }
 </script>
@@ -47,14 +141,39 @@ export default {
 @import './assets/fonts.css';
 
 #app {
-     @extend .clear;
-     font-family: $font;
-     -webkit-font-smoothing: antialiased;
-     -moz-osx-font-smoothing: grayscale;
-     height: auto;
-     width: 100%;
-     max-width: 100%;
-     @extend .flex-column;
-     justify-content: flex-start;
+	@extend .clear;
+	font-family: $font;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	height: auto;
+	width: 100%;
+	max-width: 100%;
+	@extend .flex-column;
+	justify-content: flex-start;
+
+	#nav-container {
+		font-family: $font;
+		@extend .flex-row;
+		justify-content: flex-start;
+		height: 11vh;
+		min-height: 11vh;
+		max-height: 11vh;
+		width: 100%;
+		max-width: 100%;
+		padding: 0px 20px;
+		box-sizing: border-box;
+
+		#logo {
+			height: 100px;
+		}
+		.menu-item {
+			color: white;
+			text-decoration: none;
+			transition: all 0.3s ease;
+		}
+		.menu-item:hover {
+			color: color(green);
+		}
+	}
 }
 </style>
